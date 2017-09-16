5a53e0e8bd5f9d5297a245c9da3a77c31916c132
Bastian Schwarz <bastian@codename-php.de>
2017-09-16 13:35:24 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef.cookbook.gui
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'chef.cookbook.gui'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures chef.cookbook.gui'
+long_description 'Installs/Configures chef.cookbook.gui'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/chef.cookbook.gui/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/chef.cookbook.gui'
